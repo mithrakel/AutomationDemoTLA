eaa194bac6b28cba0f0acffbb5de2346c141630f
mithrakel <dragondota@hotmail.com>
2020-08-16 10:38:10 -0400

    two

---
+++ src/test/tests/HomePageTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import base.BasePage;
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LogInPage;
+
+import java.lang.reflect.Method;
+
+public class HomePageTest extends BaseTest {
+    LogInPage logInPage;
+    HomePage homePage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void localSetUp(Method method, ITestResult result){
+        setUp(method, result);
+        logInPage = new LogInPage(getDriver());
+        homePage = new HomePage(getDriver());
+    }
+
+    @Test
+    public void validatePageHeader(){
+        logInPage.logIn("standard");
+        Assert.assertEquals(homePage.headerText.getText(), "Products");
+    }
+}
